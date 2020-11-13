35888e1d7fa103767c1102166480fe4d3a70d8d4
Sophia Guarnotta <guarnotta@chapman.edu>
2020-11-13 10:56:21 -0800

    Adds username.sh edited

--- username.sh
+++ username.sh
@@ -3,10 +3,11 @@
 # Sophia Guarnotta
 echo "Enter a Username: "
 read -r NAME
-while echo "$NAME" | grep -E -v "^[a-z]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid Username - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid Username- only user lower case letters, numbers, and _ !"
+	echo "The Username must start with a lower case letter and be between 3 and 12 characters."
+	echo "Enter a Username: "
 	read -r NAME
 done
 echo "Thank you"
